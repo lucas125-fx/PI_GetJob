6ff54d01651842747e63dc5352d0bf2d31b3b141
Lucas Costa Izidoro <lucas.izidoro@alunos.sc.senac.br>
2021-09-16 14:47:21 -0300

    main

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
-include_once(dirname(__FILE__)."/inc/MySQL.php");
+include_once(dirname(__FILE__) . "/inc/MySQL.php");
 
-include_once("./inc/header.php");
-include_once("./inc/menu.php");
+include_once(dirname(__FILE__) . "/inc/header.php");
+include_once(dirname(__FILE__) . "/inc/menu.php");
 ?>
 
 <?php
-include_once(dirname(__FILE__) . "./inc/carrosel.php");
+include_once(dirname(__FILE__) . "/inc/carrosel.php");
 echo isset($_SESSION['ID_usu']) ? $_SESSION['ID_usu'] : " NAO FOI SETADA";
 
 ?>
-<div class="bg-dark shadow-lg"><div class="text-center" style= "color: goldenrod; font-size: 4em;">Vagas disponíveis</div>
-<?php 
+<div class="bg-dark shadow-lg">
+    <div class="text-center" style="color: goldenrod; font-size: 4em;">Vagas disponíveis</div>
+    <?php
 
 
-include_once(dirname(__FILE__) . "./inc/listwork.php");
-?>
+    include_once(dirname(__FILE__) . "/inc/listwork.php");
+    ?>
 </div>
 <?php
-include_once(dirname(__FILE__) . "./inc/footer.php");
-include_once(dirname(__FILE__) . "./inc/down.php");
+include_once(dirname(__FILE__) . "/inc/footer.php");
+include_once(dirname(__FILE__) . "/inc/down.php");
 ?>
--- login.php
+++ login.php
@@ -2,9 +2,9 @@
 
 include_once(dirname(__FILE__) . "../inc/MySQL.php");
 
-include_once(dirname(__FILE__)."/inc/menu.php");
 
 include_once(dirname(__FILE__)."/inc/header.php");
+include_once(dirname(__FILE__)."/inc/menu.php");
 
 $_SESSION['email'] = "";
 
